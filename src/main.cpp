3093ea425b4ebf24e7d5212b6410e8cb474e40dc
simennb <simen.bastnes@gmail.com>
2016-09-29 15:00:11 +0200

    Do I think? Does a submarine swim?

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <armadillo>
+#include <time.h>
+
+#include "jacobi.h"
 
 using namespace std;
+using namespace arma;
 
 int main(int argc, char *argv[])
 {
--- src/project2.pro
+++ src/project2.pro
@@ -3,4 +3,10 @@ CONFIG += console c++11
 CONFIG -= app_bundle
 CONFIG -= qt
 
-SOURCES += main.cpp
+LIBS += -llapack -lblas -larmadillo
+
+SOURCES += main.cpp \
+    jacobi.cpp
+
+HEADERS += \
+    jacobi.h
